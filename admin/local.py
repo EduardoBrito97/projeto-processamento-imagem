09cd7647a57a1aa695b41cdeaf5f0b7c6074b667
Eduardo Barreto <ebb2@cin.ufpe.br>
2022-11-25 14:21:11 -0300

    Adding photos and score results

--- admin/local.py
+++ admin/local.py
@@ -3,7 +3,7 @@ class EnvironmentSettings:
         self.workspace_dir = 'C:\\Users\\Eduardo Barreto\\Desktop\\Mestrado\\Processamento de Imagem\\projeto-processamento-imagem\\admin\\..\\networks'    # Base directory for saving network checkpoints.
         self.tensorboard_dir = self.workspace_dir + '\\tensorboard\\'    # Directory for tensorboard files.
         self.pretrained_nets_dir = 'C:\\Users\\Eduardo Barreto\\Desktop\\Mestrado\\Processamento de Imagem\\projeto-processamento-imagem\\admin\\..\\pretrained_networks\\'    # Directory for pre-trained networks.
-        self.save_data_path = 'C:\\Users\\Eduardo Barreto\\Desktop\\Mestrado\\Processamento de Imagem\\projeto-processamento-imagem\\admin\\..\\results\\'    # Directory for saving network predictions for evaluation.
+        self.save_data_path = 'C:\\Users\\Eduardo Barreto\\Desktop\\Mestrado\\Processamento de Imagem\\projeto-processamento-imagem\\admin\\..\\evaluation\\burstsr\\results'    # Directory for saving network predictions for evaluation.
         self.zurichraw2rgb_dir = 'C:\\Users\\Eduardo Barreto\\Desktop\\Mestrado\\Processamento de Imagem\\projeto-processamento-imagem\\admin\\..\\databases\\zurich'    # Zurich RAW 2 RGB path
         self.burstsr_dir = 'C:\\Users\\Eduardo Barreto\\Desktop\\Mestrado\\Processamento de Imagem\\projeto-processamento-imagem\\admin\\..\\databases\\burst\\burstsr_dataset'    # BurstSR dataset path
         self.synburstval_dir = 'C:\\Users\\Eduardo Barreto\\Desktop\\Mestrado\\Processamento de Imagem\\projeto-processamento-imagem\\admin\\..\\databases\\synburst'    # SyntheticBurst validation set path
--- visualize.bat
+++ visualize.bat
@@ -1,1 +1,9 @@
-python util_scripts/visualize_synburst_val.py srgb
+del "pretrained_networks\20.pth"
+del "pretrained_networks\30.pth"
+del "pretrained_networks\40.pth"
+echo "--------------------COPYING BURSTSR PTH FILES--------------------"
+copy networks\checkpoints\dbsr\default_realworld\DBSRNet_ep0020.pth.tar pretrained_networks\20.pth
+copy networks\checkpoints\dbsr\default_realworld\DBSRNet_ep0030.pth.tar pretrained_networks\30.pth
+copy networks\checkpoints\dbsr\default_realworld\DBSRNet_ep0040.pth.tar pretrained_networks\40.pth
+python evaluation/burstsr/save_results.py dbsr_default
+python evaluation/burstsr/visualize_results.py dbsr_default
